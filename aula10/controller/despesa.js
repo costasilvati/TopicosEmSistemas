8513507e5cb45865e8c98dc8e1515956fa76b4a8
Costa-Silva <julianacostasilvati@gmail.com>
2022-05-09 22:22:02 -0300

    Código de aula 10

---
+++ aula10/controller/despesa.js
@@ -0,0 +1,18 @@
+const { json } = require('body-parser')
+const Despesa = require('../model/despesa')
+
+module.exports = app => {
+   app.get('/despesa', (req, res) => {
+       // retorna os dados??
+       dados = Despesa.lista()
+       json.toString(dados)
+      res.send()
+   })
+   app.post('/despesa', (req, res) => {
+      console.log(req.body)
+      Despesa.adiciona(req.body)
+      res.send('Você esta na rota despesa via POST')
+   })
+}
+//Testar POST - Abra o CMD no windows
+//curl -d "valor=20.0&descricao=Compras" http://localhost:3000/despesa
